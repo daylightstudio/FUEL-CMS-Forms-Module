d84b4254934efaad5baba0f497e93d5ccdeb12bc
David McReynolds <dave@thedaylightstudio.com>
2015-01-20 13:35:43 -0800

    Added mode to Multi_field_layout

--- libraries/layouts/Multi_field_layout.php
+++ libraries/layouts/Multi_field_layout.php
@@ -7,6 +7,22 @@ class Multi_field_layout extends Abstract_select_field_layout {
 	public $group = 'Forms';
 
 	// --------------------------------------------------------------------
+	
+	/**
+	 * Returns the layout's fields
+	 *
+	 * @access	public
+	 * @return	array
+	 */
+	public function fields()
+	{
+		$fields = parent::fields();
+		$fields['mode'] = array('type' => 'enum', 'options' => array('checkbox' => 'checkbox', 'select' => 'select'));
+		$fields = $this->process_fields($fields);
+		return $fields;
+	}
+	
+	// --------------------------------------------------------------------
 
 	/**
 	 * Returns an array for Form_builder to use in rendering\
