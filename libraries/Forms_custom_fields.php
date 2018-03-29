87c4410ca2b01b98b927ca22a0cf58e7b8cb9605
David McReynolds <dave@thedaylightstudio.com>
2018-03-29 14:42:13 -0700

    Added Recaptcha library so no need for composer

--- libraries/Forms_custom_fields.php
+++ libraries/Forms_custom_fields.php
@@ -1,6 +1,6 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-require_once(FORMS_PATH.'libraries/third_party/recaptchalib.php');
+require_once(FORMS_PATH.'libraries/third_party/recaptcha/src/autoload.php');
 
 class Forms_custom_fields {
 
