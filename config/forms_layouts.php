a2fef8990a3f1dae5649fb82996f689dea52c445
David McReynolds <dave@thedaylightstudio.com>
2018-09-06 12:55:39 -0700

    Removed duplicated radio form layout

--- config/forms_layouts.php
+++ config/forms_layouts.php
@@ -170,13 +170,6 @@ $config['blocks']['radio'] = array(
 	'model'    => array('forms' => 'forms_model'),
 );
 
-$config['blocks']['radio'] = array(
-	'group'    => 'Forms',
-	'class'    => 'Base_field_layout',
-	'filepath' => 'libraries/layouts',
-	'module'   => 'forms',
-	'model'    => array('forms' => 'forms_model'),
-);
 
 /* End of file MY_fuellayouts.php */
 /* Location: ./application/config/MY_fuellayouts.php */
