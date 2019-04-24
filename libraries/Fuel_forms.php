db32065703f20d87efebc90dd4392decdbca94f8
David McReynolds <dave@thedaylightstudio.com>
2019-04-24 09:46:30 -0700

    Added name parameter for return_url and form_url hidden variables

--- libraries/Fuel_forms.php
+++ libraries/Fuel_forms.php
@@ -1294,8 +1294,9 @@ class Fuel_form extends Fuel_base_library {
 			$form_fields['__antispam__'] = array_merge($form_fields['__antispam__'], $antispam_params);
 
 		}
-		$form_fields['return_url'] = array('type' => 'hidden', 'value' => $this->get_return_url());
-		$form_fields['form_url'] = array('type' => 'hidden', 'value' => current_url());
+
+		$form_fields['return_url'] = array('type' => 'hidden', 'name' => 'return_url', 'value' => $this->get_return_url());
+		$form_fields['form_url'] = array('type' => 'hidden', 'name' => 'form_url', 'value' => current_url());
 
 		// set values if any are passed
 		if (is_array($values))
